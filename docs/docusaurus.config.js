6bff044b0222fc7fa52fd8c9965085785981ac77
SophiSenne <Sophia.Silva@sou.inteli.edu.br>
2025-09-30 19:58:05 -0300

    a

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -19,16 +19,11 @@ const config = {
     v4: true, // Improve compatibility with the upcoming Docusaurus v4
   },
 
-  // Set the production url of your site here
-  url: 'https://SophiSenne.github.io',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
+  organizationName: 'SophiSenne', 
+  projectName: 'Mithril',
   baseUrl: '/Mithril/',
+  url: 'https://SophiSenne.github.io',
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
 
   onBrokenLinks: 'throw',
 
